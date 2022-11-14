8aee928175e6d166f6540e80f5c3e1be77f589aa
huynhhothoty <20110597@student.hcmute.edu.vn>
2022-11-14 13:16:55 +0700

    login

--- src/main/java/com/musicweb/controller/LoginUserServlet.java
+++ src/main/java/com/musicweb/controller/LoginUserServlet.java
@@ -29,6 +29,7 @@ public class LoginUserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
         response.sendRedirect("login.html");
+        
     }
 
     @Override
